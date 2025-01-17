fffe5c26299b721bdff8af20c90e8de45f5d7456
nadeeh16 <nadeeh16@mcmaster.ca>
2025-01-17 11:26:44 -0500

    Completed Quebecs tax rate (close #2)

--- mcmerch.py
+++ mcmerch.py
@@ -2,12 +2,24 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("price", type=float, help="The price of the item")
+    parser.add_argument(
+        "--province",
+        type=str,
+        default="Ontario",
+        choices=["Ontario", "Quebec"],
+        help="THe province where the tax is applied (default: Ontario)"
+    )
     args = parser.parse_args()
+
+    tax_rate = 1.13
+
+    if args.province == "Ontario":
+        tax_rate = 1.13
+    elif args.province == "Quebec":
+        tax_rate = 1.14975
+
     price = args.price
-
-
-
-    total = price * 1.13
+    total = price * tax_rate
 
     print(f"The total price is {total:.2f}")
 
